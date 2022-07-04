036d286d9f56dc52c6a3537c84f6e0e61fe4fd2a
Gerall09 <103030918+Gerall09@users.noreply.github.com>
2022-07-05 00:19:44 +0800

    Update config.js

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6288242147113','6282196995322']
+global.owner = ['6288242147113','6282196995322','6285891214734','6287894034440']
 global.premium = ['6288242147113','6282137340387']
 global.botname = 'SquidWard'
 global.ownername = 'Gerall'
@@ -80,7 +80,7 @@ global.mess = {
     group: '*this feature is only for groups okay*',
     private: '*this feature can only used in dm okay*',
     bot: '*Special Features for User Bot Number okay*',
-    wait: '*whoa wait zim bot processing okay*',
+    wait: '*whoa wait I processing okay*',
     endLimit: '*Brothers Daily Limit Has Expired, Type .buy limit amount limit to buy limit, Note: Limit will be reset every 12 hours*',
 }
 global.limitawal = {
